016045dd4c7dfb66eeeab5b62d6ce0f111cf3ef9
Ravit Sharma <sharma.ravit@gmail.com>
2020-12-29 21:01:13 -0800

    Work on display functions. Adding read/write functions to memory.

--- include/memory.h
+++ include/memory.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <string>
 #include <vector>
 
 #include "types.h"
@@ -12,8 +13,13 @@ public:
 
 	memory(REG16 lower, REG16 upper);
 
-	bool validLocation(uint8_t location); //indicates whether the specified address is valid location in memory
+	bool validLocation(uint16_t location); //indicates whether the specified address is valid location in memory
 
-	void write(uint8_t location, uint8_t data); // writes data to location in memory
-	uint8_t read(uint8_t location); //  reads from location in memory
+	int write(uint16_t location, uint8_t data); // writes data to location in memory
+	int read(uint16_t location, uint8_t& result); //  reads from location in memory
+
+	int write(uint16_t location, std::vector<uint8_t> data); // writes data to location in memory
+	int read(uint16_t location, int bytes, std::vector<uint8_t>& result); //  reads from location in memory
+
+	int write(uint16_t location, std::string data); // writes object code to memory
 };
